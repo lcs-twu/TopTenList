b2f950217634a894b4c66ab3a7d36015a23d3609
Tom <twu@lcs.on.ca>
2023-01-23 08:43:11 -0500

    Initial Commit

---
+++ TopTenList/TopTenListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopTenListApp.swift
+//  TopTenList
+//
+//  Created by Tom Wu on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct TopTenListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
